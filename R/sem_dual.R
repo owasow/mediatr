81099e799369b9512ad0dcbdb75f6e538afc278d
Omar Wasow <owasow@princeton.edu>
2026-03-23 14:05:14 -0600

    Remove color legend from all diagram templates, keep only p-value key

--- R/sem_dual.R
+++ R/sem_dual.R
@@ -404,9 +404,7 @@ sem_dual_med_diagram_tikz <- function(data,
 \\path[->] (x) edge node[below, align=center, yshift=-5pt] {Total: <<coef_total>>} (y);
 % Legend
 \\node[align=left, anchor=west] at (0, -8) {
-  \\scriptsize \\textcolor{<<m1_color>>}{\\rule{8pt}{2pt} paths via $M_1$} \\quad
-  \\textcolor{<<m2_color>>}{\\rule{8pt}{2pt} paths via $M_2$} \\quad
-  $^{*}p < 0.05$; $^{**}p < 0.01$; $^{***}p < 0.001$
+  \\scriptsize $^{*}p < 0.05$; $^{**}p < 0.01$; $^{***}p < 0.001$
 };
 % Label
 \\node at (-2, 7) {\\scriptsize <<diag_label>>};
@@ -496,9 +494,7 @@ sem_dual_med_diagram_compact_tikz <- function(data,
 \\node[align=center] at (6, -1.5) {Total Indirect: <<coef_total_ind>>};
 % Legend
 \\node[align=left, anchor=west] at (0, -3) {
-  \\scriptsize \\textcolor{<<m1_color>>}{\\rule{8pt}{2pt} paths via $M_1$} \\quad
-  \\textcolor{<<m2_color>>}{\\rule{8pt}{2pt} paths via $M_2$} \\quad
-  $^{*}p < 0.05$; $^{**}p < 0.01$; $^{***}p < 0.001$
+  \\scriptsize $^{*}p < 0.05$; $^{**}p < 0.01$; $^{***}p < 0.001$
 };
 % Label
 \\node at (-1.5, 7) {\\scriptsize <<diag_label>>};
@@ -749,12 +745,9 @@ sem_serial_med_diagram_tikz <- function(data,
   \\textcolor{<<m2_color>>}{Via $M_2$: <<coef_ind_m2>>}
 };
 \\node[align=center] at (12, -4) {Total Indirect: <<coef_total_ind>> \\quad Total: <<coef_total>>};
-% Color legend + p-value key
+% p-value key
 \\node[align=left, anchor=west] at (0, -5.5) {
-  \\scriptsize \\textcolor{<<m1_color>>}{\\rule{8pt}{2pt} paths via $M_1$} \\quad
-  \\textcolor{<<m2_color>>}{\\rule{8pt}{2pt} paths via $M_2$} \\quad
-  \\textcolor{<<serial_color>>}{\\rule{8pt}{2pt} $M_1 \\rightarrow M_2$ serial} \\quad
-  $^{*}p < 0.05$; $^{**}p < 0.01$; $^{***}p < 0.001$
+  \\scriptsize $^{*}p < 0.05$; $^{**}p < 0.01$; $^{***}p < 0.001$
 };
 % Label
 \\node at (-2, 9) {\\scriptsize <<diag_label>>};
